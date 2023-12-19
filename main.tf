6fba2964ea4295d9e3cdec6e00326c672685cbf1
Alex <poloz.alex@gmail.com>
2023-12-19 23:44:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,10 +49,3 @@ resource "local_file" "kubeconfig" {
   filename = "${path.module}/kubeconfig"
   file_permission = "0400"
 }
-
-module "gke_cluster" {
-  source         = "github.com/alex-poloz/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
-}
